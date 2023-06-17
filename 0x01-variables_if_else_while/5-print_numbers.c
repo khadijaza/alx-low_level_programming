6747a2576e5d07c88c05119e40ae0601b94dc877
khadijazaz <khadijazazza@gmail.com>
2023-06-17 10:40:53 -0700

    a

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,9 +5,10 @@
  * Description: print all single digit numbers
  * Return: Always 0 (Success)
 */
+
 int main(void)
 {
-int digit = 0;
+	int digit = 0;
 while (digit <= 9)
 {
 	printf("%i". digit);
